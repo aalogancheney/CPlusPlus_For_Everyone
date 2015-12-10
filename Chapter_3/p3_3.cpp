1efcc89c96ac7e84db057b72e0643d1756ddce80
Aaron Cheney <aaron.logan.cheney@gmail.com>
2015-12-10 09:10:09 -0800

    Updated problem 3 with comments and added to makefile

--- Chapter_3/p3_3.cpp
+++ Chapter_3/p3_3.cpp
@@ -1,25 +1,29 @@
-#include <iostream>
+/*
+	Write a program that reads in three floating-point numbers and prints
+	the largest of the three inputs. 
+*/
 
-using namespace std;
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-	double number1, number2, number3;
+	float number1, number2, number3;
 
-	cout << "Enter 3 numbers: ";
-	cin >> number1 >> number2 >> number3;
+	std::cout << "Enter 3 numbers: ";
+	std::cin >> number1 >> number2 >> number3;
 
+	// Three branches are used to check which of the three numbers is the largest. 
 	if(number1 >= number2 && number1 >= number3)
 	{
-		cout << number1 << " is the biggest number." << endl;
+		std::cout << number1 << " is the biggest number." << std::endl;
 	}
 	else if(number2 >= number1 && number2 >= number3)
 	{
-		cout << number2 << " is the biggest number." << endl;
+		std::cout << number2 << " is the biggest number." << std::endl;
 	}
 	else
 	{
-		cout << number3 << " is the biggest number." << endl;
+		std::cout << number3 << " is the biggest number." << std::endl;
 	}
 
 	return 0;
